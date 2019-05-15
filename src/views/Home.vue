c7c4d20ae65af5fc15034af593eeadbc5c513525
Ryan Burney <hello@ryanburney.com>
2019-05-15 12:39:46 -0600

    Fetch credit card JSON data using Axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png">
 		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<pre>{{ status }}</pre>
 	</div>
 </template>
 
 <script>
 	// @ is an alias to /src
 	import HelloWorld from "@/components/HelloWorld.vue";
+	const axios = require('axios');
 
 	export default {
 		name: "home",
 		components: {
 			HelloWorld
+		},
+		created() {
+			this.loadJSON();
+		},
+		data() {
+			return {
+				status: ""
+			};
+		},
+		methods: {
+			loadJSON() {
+				this.status = "Loading JSON...";
+				let self = this;
+				axios
+					.get(
+						// "https://jsonplaceholder.typicode.com/posts/2"
+						'http://localhost:8080/credit-cards.json'
+					)
+					.then(function(response) {
+						self.status = response.data;
+					})
+					.catch(function(error) {
+						self.status = "An error occurred: " + error;
+					});
+			}
 		}
 	};
 </script>

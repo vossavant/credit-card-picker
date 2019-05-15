75c48538e02dc83c29b534f9d26acd0c2724249c
Ryan Burney <hello@ryanburney.com>
2019-05-15 15:03:06 -0600

    Build an array of unique card types
    To be used later in a dropdown menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
-		<CreditCard v-for="card in creditCards" v-bind="card" />
+		{{creditCardTypes}}
+		<CreditCard v-for="card in creditCards" v-bind="card"/>
 		<pre>{{ creditCards }}</pre>
 	</div>
 </template>
@@ -8,10 +9,9 @@
 <script>
 	// @ is an alias to /src
 	import CreditCard from "@/components/CreditCard.vue";
-	const axios = require('axios');
+	const axios = require("axios");
 
 	export default {
-		name: "home",
 		components: {
 			CreditCard
 		},
@@ -20,21 +20,29 @@
 		},
 		data() {
 			return {
-				creditCards: {}
+				creditCards: {},
+				creditCardTypes: []
 			};
 		},
 		methods: {
 			loadCreditCardData() {
 				let self = this;
 				axios
-					.get(
-						'http://localhost:8080/credit-cards.json'
-					)
+					.get("http://localhost:8080/credit-cards.json")
 					.then(function(response) {
 						self.creditCards = response.data;
+						// self.creditCardTypes = [...new Set(response.data.map(card => card.card_type))]; // doesn't work in IE11
+						self.creditCardTypes = response.data
+							.map(card => card.card_type)
+							.filter(
+								(value, index, self) =>
+									self.indexOf(value) === index
+							);
 					})
 					.catch(function(error) {
-						self.creditCards = "Something went wrong while fetching the available credit cards: " + error;
+						self.creditCards =
+							"Something went wrong while fetching the available credit cards: " +
+							error;
 					});
 			}
 		}

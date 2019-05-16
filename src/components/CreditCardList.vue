216d400cefbf339130dd2a1483d13afbe7d4e9e0
Ryan Burney <hello@ryanburney.com>
2019-05-16 13:35:14 -0600

    Move CTA text below matching offers

--- src/components/CreditCardList.vue
+++ src/components/CreditCardList.vue
@@ -2,11 +2,12 @@
 	<div>
 		<h3>Type: {{ card_type }} // Credit: {{ credit_rating }}</h3>
 		<p v-if="intro_text">{{ intro_text }}</p>
-		<p v-if="cta_text" v-html="cta_text"></p>
 		<p v-if="error_text" v-html="error_text"></p>
 
 		<CreditCard :card="card1" v-if="card1"/>
 		<CreditCard :card="card2" v-if="card2"/>
+		
+		<p v-if="cta_text" v-html="cta_text"></p>
 	</div>
 </template>
 

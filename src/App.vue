8e63f2ad02d647f847992aefa6eb9757566a38b5
Ryan Burney <hello@ryanburney.com>
2019-05-16 09:08:55 -0600

    Load types and ratings into dropdowns

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
 	<div>
-		<h4>Unique Card Types:</h4>
-		{{ creditCardTypes }}
-		
-		<h4>Unique Credit Ratings:</h4>
-		{{ creditCardRatings }}
-		
-		<pre>{{ creditCards[1] }}</pre>
-		
-		<CreditCard v-for="card in creditCards" v-bind="card" />
+		<h1>Credit Card Picker Doohickey</h1>
+		<p>We've vetted hundreds of credit cards to bring you the select few leaders in their categories. Check out our top picks by category, some of which are from our partners, to find the best credit card to suit your needs.</p>
+		<div class="d-flex">
+			<div v-if="creditCardTypes.length">
+				<h4>Search by Card Type</h4>
+				<select name="" id="">
+					<option v-for="type in creditCardTypes" :value="type">
+						{{ type }}
+					</option>
+				</select>
+			</div>
+			<div v-if="creditCardRatings.length">
+				<h4>Search by Credit Rating</h4>
+				<select name="" id="">
+					<option v-for="rating in creditCardRatings" :value="rating">
+						{{ rating }}
+					</option>
+				</select>
+			</div>
+		</div>
+		<div>
+			<pre>{{ creditCards[1] }}</pre>
+			
+			<CreditCard v-for="card in creditCards" v-bind="card" />
+		</div>
 	</div>
 </template>
 

d7f1d9bca5b1c93918db09286837a439d7ec369b
Ryan Burney <hello@ryanburney.com>
2019-05-16 23:03:25 -0600

    Add hover effect for button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -35,5 +35,13 @@ export default {
         font-weight: 700;
         line-height: 3rem;
         padding: 0 3rem;
+        transition: background 250ms ease-in-out, box-shadow 250ms ease-in-out, transform 250ms ease-in-out;
+
+        &:hover {
+            background: darken($ascent_secondary_cta, 5%);
+            box-shadow: 0 0.25rem 0.5rem transparentize(black, 0.8);
+            transform: translate3d(-2px, -2px, 0);
+            text-decoration: none;
+        }
     }
 </style>

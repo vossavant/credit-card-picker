9709ae9e79ec7316b6256157e0f6906dee31932b
Ryan Burney <hello@ryanburney.com>
2019-05-17 14:58:40 -0600

    Add Ascent logo to intro

--- src/components/TheIntro.vue
+++ src/components/TheIntro.vue
@@ -1,6 +1,7 @@
 <template>
     <header>
-        <h1>Credit Card Picker Doohickey</h1>
+        <img src="@/assets/logo@2x.png" width="200">
+        <h1>Credit Card Picker</h1>
         <p class="intro-p">We've vetted hundreds of credit cards to bring you the select few leaders in their categories. Check out our top picks by category, some of which are from our partners, to find the best credit card to suit your needs.</p>
     </header>
 </template>
